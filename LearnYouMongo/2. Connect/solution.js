728ef45102218660b6db4e5425c1be80c8117667
“John <johnjamesmartin@live.com>
2016-10-03 00:40:45 +0100

    Exercise 2: Connect

---
+++ LearnYouMongo/2. Connect/solution.js
@@ -0,0 +1,7 @@
+/*
+    No code to be written for this exercise. Take the following steps:
+    -----------------------------------------------------------------
+    1. Create new 'data' folder in root folder: 'mkdir data'.
+    2. Run 'mongod --port 27017 --dbpath=./data --nojournal' ('--nojournal' suffix is important when using Cloud 9 IDE (or command won't run)).
+    3. Open new tab and 'leanyounode verify'.
+*/
