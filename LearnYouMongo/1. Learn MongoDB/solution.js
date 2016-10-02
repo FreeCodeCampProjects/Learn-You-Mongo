f3afa4890c08f6f02e58d98d75fa9de3ac461eaf
“John <johnjamesmartin@live.com>
2016-10-03 00:31:23 +0100

    Exercise 1: Learn MongoDB

---
+++ LearnYouMongo/1. Learn MongoDB/solution.js
@@ -0,0 +1,7 @@
+/*
+    No code to be written for this exercise. Take the following steps:
+    -----------------------------------------------------------------
+    1. Ran 'sudo apt-get install mongodb-org' to install MongoDB on 'Cloud 9' IDE.
+    2. Created 'solution.js'
+    3. Ran 'learnyoumongo verify'
+*/
